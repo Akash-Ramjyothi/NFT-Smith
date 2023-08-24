bae694247e32699b8b92b51337da8b61584fab5f
Akash Ramjyothi <akash.ramjyothi@gmail.com>
2023-08-25 01:27:34 +0530

    Completed 1st page Black side

--- src/App.js
+++ src/App.js
@@ -11,12 +11,20 @@ function App() {
     <div className="App">
       <div className="white-container"></div>
       <div className="black-container">
-        <Image width={529} height={459} src="./Exportable Logo_4x.png" />
-
-        <Image width={20} height={20} src="./GitHubLogo.svg" />
-        <Link underline href="https://github.com/Akash-Ramjyothi/NFT-Smith" target="_blank">
-          Source Code
-        </Link>
+        <div className="black-items-wrapper">
+          <Image width={529} height={459} src="./Exportable Logo_4x.png" />
+        </div>
+        <div className="hyperlink-wrapper">
+          <Image width={20} height={20} src="./GitHubLogo.svg" />
+          <div className="paddingRight-10" />
+          <Link
+            underline
+            href="https://github.com/Akash-Ramjyothi/NFT-Smith"
+            target="_blank"
+          >
+            Source Code
+          </Link>
+        </div>
       </div>
     </div>
   );
